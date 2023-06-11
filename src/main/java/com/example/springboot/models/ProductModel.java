f9c0d6a7e378c2518f5be90cb1b37f0cbc5e987a
ninho <ninho5@live.com>
2023-06-11 16:09:48 -0300

    atualização nova

--- src/main/java/com/example/springboot/models/ProductModel.java
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 @Table(name = "TB_PRODUCTS")
 public class ProductModel implements Serializable {
     private static final long serialVersionUID = 1L;
-//sdss
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID idProduct;
